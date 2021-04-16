6a8ad0acaae22fa934f328c0811161622221f26b
NelsonCheung <940991267@qq.com>
2021-04-16 21:57:37 +0800

    fix bug in lab5 stdlib.cpp: itos

--- lab5/src/3/src/utils/stdlib.cpp
+++ lab5/src/3/src/utils/stdlib.cpp
@@ -9,8 +9,6 @@ void swap(T &x, T &y) {
 
 
 void itos(char *numStr, uint32 num, uint32 mod) {
-    numStr[0] = '\0';
-
     // 只能转换2~26进制的整数
     if (mod < 2 || mod > 26 || num < 0) {
         return;
@@ -37,4 +35,6 @@ void itos(char *numStr, uint32 num, uint32 mod) {
     for(int i = 0, j = length - 1; i < j; ++i, --j) {
         swap(numStr[i], numStr[j]);
     }
+    
+    numStr[length] = '\0';
 }
